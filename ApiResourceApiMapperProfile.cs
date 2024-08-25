18e1b4e6bbb7d3fde2f65f87d794cec651ee1ffb
milisha <milisha28@gmail.com>
2024-08-25 14:06:57 -0400

    First Commit

---
+++ ApiResourceApiMapperProfile.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using binovi.connect.Admin.Api.Dtos.ApiResources;
+using Skoruba.IdentityServer4.Admin.BusinessLogic.Dtos.Configuration;
+
+namespace binovi.connect.Admin.Api.Mappers
+{
+    public class ApiResourceApiMapperProfile : Profile
+    {
+        public ApiResourceApiMapperProfile()
+        {
+            // binovi.connect.api Resources
+            CreateMap<ApiResourcesDto, ApiResourcesApiDto>(MemberList.Destination)
+                .ReverseMap();
+
+            CreateMap<ApiResourceDto, ApiResourceApiDto>(MemberList.Destination)
+                .ReverseMap();
+
+            // binovi.connect.api Scopes
+            CreateMap<ApiScopesDto, ApiScopesApiDto>(MemberList.Destination)
+                .ReverseMap();
+
+            CreateMap<ApiScopeDto, ApiScopeApiDto>(MemberList.Destination)
+                .ReverseMap();
+            
+            CreateMap<ApiScopesDto, ApiScopeApiDto>(MemberList.Destination)
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ApiScopeId))
+                .ReverseMap();
+
+            // binovi.connect.api Secrets
+            CreateMap<ApiSecretsDto, ApiSecretApiDto>(MemberList.Destination)
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ApiSecretId))
+                .ReverseMap();
+
+            CreateMap<ApiSecretDto, ApiSecretApiDto>(MemberList.Destination);
+            CreateMap<ApiSecretsDto, ApiSecretsApiDto>(MemberList.Destination);
+
+            // binovi.connect.api Properties
+            CreateMap<ApiResourcePropertiesDto, ApiResourcePropertyApiDto>(MemberList.Destination)
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ApiResourcePropertyId))
+                .ReverseMap();
+
+            CreateMap<ApiResourcePropertyDto, ApiResourcePropertyApiDto>(MemberList.Destination);
+            CreateMap<ApiResourcePropertiesDto, ApiResourcePropertiesApiDto>(MemberList.Destination);
+        }
+    }
+}
+
+
+
+
+
---
+++ MediaController.cs
@@ -0,0 +1,74 @@
+﻿using api.Models.Response;
+using binovi.cloud.api.models.Models.Request;
+using binovi.cloud.azure.Enums;
+using binovi.cloud.Enums;
+using binovi.cloud.Exceptions;
+using binovi.cloud.Interfaces;
+using binovi.cloud.Models.Request;
+using binovi.cloud.Models.Response;
+using binovi.cloud.Utils;
+using ehapi.Authentication;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace binovi.cloud.Controllers
+{
+    /// <summary>
+    /// Manage Patients for an Organization
+    /// </summary>
+
+    [ClaimsAuthorize(ClaimType.Developer, ClaimType.AccessConsumerApp, ClaimType.AdministerAdministrators)]
+    [RoutePrefix("api/mediaFiles")]
+    public class MediaController : ApiController
+    {
+        #region Dependency Injection
+        private IMediaFileRepo _repository;
+
+        public MediaController(IMediaFileRepo repository)
+        {
+            _repository = repository;
+		}
+
+        #endregion
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ResponseType(typeof(BaseResponse<string>))]
+        [Route("token")]
+        public IHttpActionResult GetMediaToken(int mediaFileId)
+        {
+            BaseResponse<string> result = null;
+
+            try
+            {
+                string environment = ConfigurationUtils.ApiEnvironment;
+                result = _repository.GetMediaToken(environment, mediaFileId);
+
+                return new HttpActionResultEC<string>(Request, result);
+            }
+            catch (Exception ex)
+            {
+                return new HttpActionResultError(ex, Request);
+            }
+        }
+
+    }
+}
